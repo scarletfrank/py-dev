55453bd0c2efb55698311c598d639474cd9ef438
FrankScarlet <hferfrank@gmail.com>
2021-02-14 00:47:29 +0800

    如果timeout不行，再使用max_instances 增加并发

--- schedule-bot/app/cron.py
+++ schedule-bot/app/cron.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BlockingScheduler
 import subprocess as sub
+import logging
 
-x = 513
+x = 515
 
 # 输出时间
 
@@ -11,23 +12,41 @@ def job():
     print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 # 每日测试一下地址
+
+
 def daily():
-    sub.run(["ffmpeg", "-y", "-i", "https://fms2.uniqueradio.jp/agqr10/aandg1.m3u8",
-             "-t", "60", f"mediafiles/daily.mp4"], check=True)
+    try:
+        sub.run(["ffmpeg", "-y", "-i", "https://fms2.uniqueradio.jp/agqr10/aandg1.m3u8",
+                 "-t", "60", f"mediafiles/daily.mp4"], check=True, timeout=80)
+    except sub.TimeoutExpired:
+        print("into timeout exception")
+        logging.info("Daily Timeout, proc should be terminated")
 
 # 周日 晚上七点半（日本时间）
+
+
 def botjob():
     global x
     # 避免403，提前触发，录稍微久一点，之后才trim
-    sub.run(["ffmpeg", "-y", "-i", "https://fms2.uniqueradio.jp/agqr10/aandg1.m3u8",
-             "-t", "1860", f"mediafiles/{x}.mp4"], check=True)
-    x += 1
+    try:
+        sub.run(["ffmpeg", "-y", "-i", "https://fms2.uniqueradio.jp/agqr10/aandg1.m3u8",
+                 "-t", "1860", f"mediafiles/{x}_fresh.mp4"], check=True, timeout=1900)
+        x += 1
+    except sub.TimeoutExpired:
+        print("into timeout exception")
+        logging.info("Daily Timeout, proc should be terminated")
+
 
 # 周六早上十点半（日本时间）
 def backup():
     global x
-    sub.run(["ffmpeg", "-y", "-i", "https://fms2.uniqueradio.jp/agqr10/aandg1.m3u8",
-             "-t", "1860", f"mediafiles/{x-1}.mp4"], check=True)
+    try:
+        sub.run(["ffmpeg", "-y", "-i", "https://fms2.uniqueradio.jp/agqr10/aandg1.m3u8",
+                 "-t", "1860", f"mediafiles/{x-1}_bk.mp4"], check=True, timeout=1900)
+    except sub.TimeoutExpired:
+        print("into timeout exception")
+        logging.info("Daily Timeout, proc should be terminated")
+
 
 # scheduler = BackgroundScheduler()
 scheduler = BlockingScheduler()
--- schedule-bot/cron-test.py
+++ schedule-bot/cron-test.py
@@ -1,22 +1,31 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BlockingScheduler
 import subprocess as sub
+import logging
+
 
 x = 513
-
+# logging.basicConfig(level=logging.INFO)
 # 输出时间
 
 
 def job():
-    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    s = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(s)
+    logging.info(f"Time: {s}")
 
 
 # 每日测试一下地址
 def daily():
-    sub.run(["ffmpeg", "-y", "-i", "https://fms2.uniqueradio.jp/agqr10/aandg1.m3u8",
-             "-t", "60", f"daily.mp4"], check=True)
+    try:
+        sub.run(["ffmpeg", "-y", "-i", "https://fms2.uniqueradio.jp/agqr10/aandg1.m3u8",
+                 "-t", "60", f"output/daily.mp4"], check=True, timeout=80)
+    except sub.TimeoutExpired:
+        print("into timeout exception")
+        logging.info("Daily Timeout, proc should be terminated")
+        # 周日 晚上七点半（日本时间）
 
-# 周日 晚上七点半（日本时间）
+
 def botjob():
     global x
     # 避免403，提前触发，录稍微久一点，之后才trim
@@ -25,6 +34,8 @@ def botjob():
     x += 1
 
 # 周六早上十点半（日本时间）
+
+
 def backup():
     global x
     sub.run(["ffmpeg", "-y", "-i", "https://fms2.uniqueradio.jp/agqr10/aandg1.m3u8",
@@ -33,9 +44,9 @@ def backup():
 
 # scheduler = BackgroundScheduler()
 scheduler = BlockingScheduler()
-scheduler.add_job(job, 'cron', second='*/59')
-scheduler.add_job(daily, 'cron', minute=20)
-scheduler.add_job(backup, 'cron', day_of_week='6', hour=11, minute=38)
-scheduler.add_job(botjob, 'cron', day_of_week='6', hour=11, minute=38)
+scheduler.add_job(job, 'cron', second='*/5')
+scheduler.add_job(daily, 'cron', minute='*/3')
+# scheduler.add_job(backup, 'cron', day_of_week='6', hour=11, minute=38)
+# scheduler.add_job(botjob, 'cron', day_of_week='6', hour=11, minute=38)
 
 scheduler.start()  # 输出时间
