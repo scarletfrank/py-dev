df9d4498206cb6eb8d78f7f88123c2acf8252608
FrankScarlet <hferfrank@gmail.com>
2021-05-25 18:40:09 +0800

    add flask + neo4j docker

--- flask-newbie/README.md
+++ flask-newbie/README.md
@@ -2,7 +2,7 @@
 
 > 受到Neo4j Python Demo的启发，看到2.0.0版本刚出，当然是学新不学旧
 > [Neo4j-py-bolt](https://github.com/neo4j-examples/movies-python-bolt)
-
+> 再结合一下[Flaskr](https://flask.palletsprojects.com/en/2.0.x/tutorial/layout/)
 
 ## 依赖
 
--- flask-newbie/app/flaskr/hello.py
+++ flask-newbie/app/flaskr/hello.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from markupsafe import escape
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# @app.route("/<name>")
+# def hello(name):
+#     return f"Hello, {escape(name)}!"
+
+
+# Use Jinja to do the HTML escaping
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
